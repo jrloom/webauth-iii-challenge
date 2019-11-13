6112f5debc17d1b4094e815a4b23b26933162040
Jason Loomis <jason@jrloomis.com>
2019-11-13 16:08:17 -0500

    server running

---
+++ index.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+console.log(process.env.MSG);
+
+const server = require("./server");
+
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => console.log(`server listening on port ${port}`));
