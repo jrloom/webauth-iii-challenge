631c37333ec898327d05f218d32f21d49c9b90d1
Jason Loomis <jason@jrloomis.com>
2019-11-14 07:39:50 -0500

    users only accessible if authenticated, users retrieved from db and listed

--- app/src/Users.js
+++ app/src/Users.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { axiosWithAuth } from "./Utils/AxiosWithAuth";
 
 const Users = () => {
+  const [user, setUser] = useState([]);
+  const token = localStorage.getItem("token");
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get("/users")
+      .then(res => {
+        console.log(`success > `, res);
+        setUser(res.data);
+      })
+      .catch(err => console.log(`fail > `, err.message));
+  }, []);
+
   return (
     <div>
-      <div>Users</div>
+      {user.map(user => {
+        return (
+          <div key={user.id}>
+            <div>{user.username}</div>
+          </div>
+        );
+      })}
     </div>
   );
 };
